63836d5feb038caa2d3849c6d89496506862e314
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-27 19:33:52 +0900

    O2F-509 [FEAT] 게시글 목록 조회 API Call 로직 작성

---
+++ src/services/ootd/PostService.ts
@@ -0,0 +1,27 @@
+import { authAxiosInstance } from '@/apis/utils'
+import type { PostPageResponse, PostResponse } from '@/services/ootd/PostDto'
+import type { AxiosResponse } from 'axios'
+import { AxiosError } from 'axios'
+
+const SNS_SERVICE_PREFIX_PATH = '/sns-service'
+
+export const getPosts = async (page: number, size: number, sort: string): Promise<PostPageResponse<PostResponse>> => {
+  try {
+    const response: AxiosResponse = await authAxiosInstance.get(`/posts?page=${page}&size=${size}&sort=${sort}`)
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        if (error.response.status >= 400) {
+          alert(error.response.data.message)
+          console.error(`Client Error=${error.response.data.message}`)
+        }
+        if (error.response.status < 500) {
+          alert('서버 내부 오류')
+          console.error('Internal Server Error')
+        }
+      }
+    }
+    throw error
+  }
+}
