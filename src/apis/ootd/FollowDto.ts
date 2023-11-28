b83b6a2d7d5c9750adae745ee941d22a7f32dbc3
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-28 16:58:31 +0900

    O2F-574 [FEAT] 팔로잉 목록 조회 API Call 로직 작성

--- src/apis/ootd/FollowDto.ts
+++ src/apis/ootd/FollowDto.ts
@@ -10,3 +10,15 @@ export interface FollowerResponse {
   profileImgUrl: string;
   isFollowing: boolean;
 }
+
+export interface FollowingPageResponse<T extends FollowingResponse> {
+  totalPages: number;
+  totalElements: number;
+  followings: Array<T>;
+}
+
+export interface FollowingResponse {
+  id: number;
+  nickname: string;
+  profileImgUrl: string;
+}
