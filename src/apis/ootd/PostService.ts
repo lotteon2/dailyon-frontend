fd0ddd6f523935fa23b4549c587679f03b072290
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-29 13:44:19 +0900

    O2F-550 [FEAT] 게시글 좋아요 목록 조회 및 내 게시글 조회 API Call 로직 작성

--- src/apis/ootd/PostService.ts
+++ src/apis/ootd/PostService.ts
@@ -1,5 +1,11 @@
 import { authAxiosInstance } from '@/apis/utils'
-import type { PostPageResponse, PostResponse } from '@/apis/ootd/PostDto'
+import type {
+  MyPostPageResponse,
+  MyPostResponse,
+  PostLikePageResponse, PostLikeResponse,
+  PostPageResponse,
+  PostResponse
+} from '@/apis/ootd/PostDto'
 import type { AxiosResponse } from 'axios'
 import { AxiosError } from 'axios'
 
@@ -8,6 +14,60 @@ const SNS_SERVICE_PREFIX_PATH = '/sns-service'
 export const getPosts = async (page: number, size: number, sort: string): Promise<PostPageResponse<PostResponse>> => {
   try {
     const response: AxiosResponse = await authAxiosInstance.get(`/posts`, {
+      params: {
+        page: page,
+        size: size,
+        sort: sort
+      }
+    })
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        if (error.response.status >= 400) {
+          alert(error.response.data.message)
+          console.error(`Client Error=${error.response.data.message}`)
+        }
+        if (error.response.status < 500) {
+          alert('서버 내부 오류')
+          console.error('Internal Server Error')
+        }
+      }
+    }
+    throw error
+  }
+}
+
+export const getMyPosts = async (page: number, size: number, sort: string): Promise<MyPostPageResponse<MyPostResponse>> => {
+  try {
+    const response: AxiosResponse = await authAxiosInstance.get(`/my-posts`, {
+      params: {
+        page: page,
+        size: size,
+        sort: sort
+      }
+    })
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        if (error.response.status >= 400) {
+          alert(error.response.data.message)
+          console.error(`Client Error=${error.response.data.message}`)
+        }
+        if (error.response.status < 500) {
+          alert('서버 내부 오류')
+          console.error('Internal Server Error')
+        }
+      }
+    }
+    throw error
+  }
+}
+
+export const getPostLikes = async (page: number, size: number, sort: string): Promise<PostLikePageResponse<PostLikeResponse>> => {
+  try {
+    const response: AxiosResponse = await authAxiosInstance.get(`/posts/likes`, {
       params: {
         page: page,
         size: size,
