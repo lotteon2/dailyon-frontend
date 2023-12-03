0a58df13cae76112baeb37788f2eee4816c7c56f
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-03 17:47:35 +0900

    [CHORE] 미리 생성해둔 게시글 상세조회 API Call 로직 추가

--- src/apis/ootd/PostService.ts
+++ src/apis/ootd/PostService.ts
@@ -1,7 +1,7 @@
 import { authAxiosInstance } from '@/apis/utils'
 import type {
   OOTDPostPageResponse,
-  OOTDPostResponse, PostCreateRequest, PostCreateResponse, PostImageProductDetailCreateRequest,
+  OOTDPostResponse, PostCreateRequest, PostCreateResponse, PostDetailResponse, PostImageProductDetailCreateRequest,
   PostLikePageResponse, PostLikeResponse,
   PostPageResponse,
   PostResponse
@@ -126,6 +126,28 @@ export const createPost = async (postCreateRequest: PostCreateRequest<PostImageP
   try {
     const response: AxiosResponse = await authAxiosInstance.post(`/posts`, postCreateRequest)
     return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        if (error.response.status >= 400) {
+          alert(error.response.data.message)
+          console.error(`Client Error=${error.response.data.message}`)
+        }
+        if (error.response.status < 500) {
+          alert('서버 내부 오류')
+          console.error('Internal Server Error')
+        }
+      }
+    }
+    throw error
+  }
+}
+
+export const getPostDetail = async (postId: number)
+  : Promise<PostDetailResponse> => {
+  try {
+    const response: AxiosResponse = await authAxiosInstance.get(`/posts/${postId}`)
+    return response.data.post
   } catch (error) {
     if (error instanceof AxiosError) {
       if (error.response) {
