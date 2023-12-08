e11ba60acf4c3c2b32ae400f6a17c8f49e4e3d4e
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-08 10:42:10 +0900

    O2F-595 [REFACTOR] 마이페이지 반응형 페이지 제작

--- src/components/MyPageHeaderComponent.vue
+++ src/components/MyPageHeaderComponent.vue
@@ -95,28 +95,22 @@ onBeforeUnmount(() => {
 
 <template>
   <div class="user-container">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="150"
-      height="150"
-      viewBox="0 0 150 150"
-      fill="none"
-    >
-      <circle cx="75" cy="75" r="75" fill="#D9D9D9" />
-    </svg>
-    <div class="user-name-container">
-      <div class="user-name-container-first-line">
-        <h1>
-          닉네임&nbsp;
-          <h2>님, 반갑습니다!</h2>
-        </h1>
+    <div class='user-info-container'>
+      <img class='user-profile-img' src='@/assets/images/default-profile-image.png' />
+      <div class="user-name-container">
+        <div class="user-name-container-first-line">
+          <h1>
+            닉네임&nbsp;
+            <h2>님, 반갑습니다!</h2>
+          </h1>
+        </div>
+        <h3>이메일 주소</h3>
       </div>
-      <h3>이메일 주소</h3>
     </div>
     <div class="user-container-point-container">
-      포인트
-      <h1>0</h1>
-      점
+      <div class='point-wrapper'>
+        포인트<h1>0</h1>점
+      </div>
       <button class="payment-modal-button" @click="open">결제하기</button>
       <div v-if="displayModal" class="payment-modal">
         <div class="modal-content">
@@ -139,8 +133,10 @@ onBeforeUnmount(() => {
         </div>
       </div>
     </div>
-    <div class="modify-button">정보 수정</div>
-    <div class="logout-button">로그아웃</div>
+    <div class='user-info-change-btn-wrapper'>
+      <div class="modify-button">정보 수정</div>
+      <div class="logout-button">로그아웃</div>
+    </div>
   </div>
 </template>
 
