58cb361edecbf22c6e809d6201b86a10c651a2e9
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-10 18:08:50 +0900

    O2F-594 [FEAT] 게시글 조회수 로직 구현

--- src/apis/ootd/PostService.ts
+++ src/apis/ootd/PostService.ts
@@ -207,3 +207,24 @@ export const getPostDetail = async (postId: number)
     throw error
   }
 }
+
+export const addViewCount = async (postId: number)
+  : Promise<void> => {
+  try {
+    await authAxiosInstance.put(`${SNS_SERVICE_PREFIX_PATH}/posts/${postId}/view-count`)
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        if (error.response.status >= 400) {
+          alert(error.response.data.message)
+          console.error(`Client Error=${error.response.data.message}`)
+        }
+        if (error.response.status < 500) {
+          alert('서버 내부 오류')
+          console.error('Internal Server Error')
+        }
+      }
+    }
+    throw error
+  }
+}
