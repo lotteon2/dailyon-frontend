9e018fda9cf7310310212d1c7a47e6444f0ca1ba
wakkpu <ho0524870@gmail.com>
2023-12-18 20:49:19 +0900

    O2F-474 [FEAT] css 수정

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
+import type { Category } from '@/apis/category/CategoryDto'
+import { getBreadCrumbs } from '@/apis/category/CategoryClient'
 import type { AxiosResponse } from 'axios'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import type { ReadProductResponse, ReadProductSliceResponse } from '@/apis/product/ProductDto'
 import { getProductSlice } from '@/apis/product/ProductClient'
-import BreadCrumbComponent from '@/components/product/BreadCrumbComponent.vue'
 
 const VITE_STATIC_IMG_URL = ref<string>(import.meta.env.VITE_STATIC_IMG_URL)
 
-const route = useRoute()
+const breadCrumbs = ref<Category[]>([])
 
-const categoryId = ref<number>(0)
-const brandId = ref<number>(0)
-const gender = ref<string>('')
-const type = ref<string>('')
+const route = useRoute()
+const router = useRouter()
+
+const categoryId = ref<number | null>(null)
+const brandId = ref<number | null>(null)
+const gender = ref<string | null>(null)
+const type = ref<string | null>(null)
 
 const hasNext = ref<boolean>(true)
 const lastId = ref<number>(0)
@@ -31,7 +35,6 @@ const initData = () => {
   if (route.query.type) {
     type.value = String(route.query.type)
   }
-
   if (route.query.category) {
     categoryId.value = Number(route.query.category)
   }
@@ -48,29 +51,71 @@ const initData = () => {
     })
 }
 
-onBeforeMount(initData)
+onMounted(initData)
+
+/*watch([categoryId, brandId, gender, type, lastId], (nv, ov) => {
+  if (nv.some((value, index) => value !== ov[index])) {
+    getProductSlice(lastId.value, brandId.value, categoryId.value, gender.value, type.value)
+      .then((axiosResponse: AxiosResponse) => {
+        const response: ReadProductSliceResponse = axiosResponse.data
+        hasNext.value = response.hasNext
+        lastId.value = response.productResponses[response.productResponses.length - 1].id
+        products.value = [...products.value, ...response.productResponses]
+      })
+      .catch((error: any) => {
+        alert(error.response!.data!.message)
+      })
+  }
+})*/
 </script>
 
 <template>
   <div style="width: 50vw">
-    <BreadCrumbComponent :category="categoryId" />
+    <div class="breadcrumb-container">
+      <a href="/" style="font-family: TheJamsil">홈</a>
+      <div class="breadcrumbs" v-for="category in breadCrumbs">
+        <p style="padding-right: 5px; padding-left: 5px">></p>
+        <a :href="`/product-list?category=${category.id}&type=NORMAL`">
+          {{ category.name }}
+        </a>
+      </div>
+    </div>
     <div class="product-list-container">
-      <RouterLink
-        class="prod-info"
-        v-for="(product, index) in products"
-        :to="`/products/${product.id}`"
-        :key="product.id"
-      >
+      <div v-for="(product, index) in products" :key="product.id" class="prod-info">
         <img :src="`${VITE_STATIC_IMG_URL}${product.imgUrl}`" alt="productImg" />
         <h1>{{ product.brandName }}</h1>
         <h2>{{ product.name }}</h2>
         <h3>{{ product.price }}원</h3>
-      </RouterLink>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.breadcrumb-container {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-items: center;
+
+  padding-left: 10px;
+  padding-bottom: 20px;
+
+  width: 100%;
+}
+
+.breadcrumbs {
+  display: flex;
+  align-items: flex-start;
+  justify-items: center;
+
+  font-family: TheJamsil;
+}
+
+.breadcrumbs > a {
+  color: inherit;
+  text-decoration: none;
+}
 .product-list-container {
   display: flex;
   flex-direction: row;
@@ -83,8 +128,8 @@ onBeforeMount(initData)
 .prod-info {
   display: flex;
   flex-direction: column;
-  flex-basis: calc(25% - 40px);
-  margin: 20px;
+  flex-basis: calc(25% - 20px);
+  margin: 10px;
 }
 
 .prod-info > h1 {
