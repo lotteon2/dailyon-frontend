53b61864e202082ba1ccbdfa44a22dfb85a68f4d
wakkpu <ho0524870@gmail.com>
2023-12-19 12:20:42 +0900

    [FIX] 상품 상세 이미지 컴포넌트화

---
+++ src/components/product/DescribeImageComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const VITE_STATIC_IMG_URL = ref<string>(import.meta.env.VITE_STATIC_IMG_URL)
+
+const props = defineProps({
+  describeImgUrls: {
+    type: [],
+    required: false
+  },
+  isDescribeImages: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <img
+    v-if="props.describeImgUrls.length > 0"
+    v-for="url in props.describeImgUrls"
+    :src="`${VITE_STATIC_IMG_URL}${url}`"
+    alt="describeImg"
+  />
+</template>
+
+<style scoped></style>
