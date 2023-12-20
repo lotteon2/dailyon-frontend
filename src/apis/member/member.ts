88c205e88c73c1d65996acf9b04f8b28ca00f50c
AMolRang <rlghks131@korea.ac.kr>
2023-12-20 20:27:31 +0900

    O2F-943 [Feat] 페이지네이션 적용

--- src/apis/member/member.ts
+++ src/apis/member/member.ts
@@ -1,3 +1,39 @@
-import { authAxiosInstance } from '@/apis/utils'
-import { AxiosError } from 'axios'
-import type { MemberInfoDto } from './MemberDto'
+import { type AxiosResponse } from "axios";
+import { authAxiosInstance } from "@/apis/utils";
+import { useMemberStore } from '@/stores/member/MemberStore';
+
+
+const MEMBER_SERVICE_PREFIX: string = "/member-service"
+const memberStore = useMemberStore();
+
+
+export const getMember = async () => {
+    try {
+      const response = await authAxiosInstance.get(`${MEMBER_SERVICE_PREFIX}/members`);
+      memberStore.setMemberInfo(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('API 호출 중 오류 발생:', error);
+    }
+  };
+
+
+
+  
+
+
+  export const getMemberAddressT = async (page : Number) => {
+    try {
+      const response = await authAxiosInstance.get('/member-service/addresses/test', {
+        params: {
+          page: page,
+          size: 5,
+        },
+      });
+      console.log("페이지네이션 어드레스");
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('API 호출 중 오류 발생:', error);
+    }
+  };
