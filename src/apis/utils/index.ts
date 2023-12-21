89bfc83e5aa0155a0da48740e94635349ff4336d
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-21 14:50:35 +0900

    [FIX] type error 해결 및 CI/CD 문서 수정

--- src/apis/utils/index.ts
+++ src/apis/utils/index.ts
@@ -1,4 +1,4 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { type AxiosInstance, type InternalAxiosRequestConfig } from 'axios'
 
 // vite에서 환경변수 값을 가져오기 위해서 import.meta 를 사용함
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
@@ -13,14 +13,13 @@ const axiosApi = (baseURL: string) => {
   // TODO: 로그인 정보 저장 및 API에 인증 토큰 붙이는 코드
   instance.interceptors.request.use((config) => {
     const accessToken = localStorage.getItem('accessToken')
-    if(accessToken !== null) {
-      // localStorage에 accessToken이 존재할 때만 헤더에 추가
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`
-      }
 
-      return config
+    // localStorage에 accessToken이 존재할 때만 헤더에 추가
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
     }
+
+    return config
   })
   return instance
 }
