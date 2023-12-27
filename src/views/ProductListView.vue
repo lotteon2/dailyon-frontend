9b76f02634867d1a5588e4b388a20c6c9c7d7e44
wakkpu <ho0524870@gmail.com>
2023-12-27 21:43:40 +0900

    O2F-996 [FIX] 디자인 수정

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -64,7 +64,20 @@ onBeforeMount(initData)
         <img :src="`${VITE_STATIC_IMG_URL}${product.imgUrl}`" alt="productImg" />
         <h1>{{ product.brandName }}</h1>
         <h2>{{ product.name }}</h2>
-        <h3>{{ product.price }}원</h3>
+        <div class="product-third-info">
+          <div class="product-aggregate">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="20" height="20">
+              <path
+                fill="black"
+                d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+              />
+            </svg>
+            <h1>{{ product.avgRating.toFixed(1) }} | ({{ product.reviewCount }})</h1>
+          </div>
+          <div>
+            <h3>{{ product.price }}원</h3>
+          </div>
+        </div>
       </RouterLink>
     </div>
   </div>
@@ -105,16 +118,20 @@ onBeforeMount(initData)
   line-height: 30px;
 }
 
-.prod-info > h3 {
+.product-third-info {
   display: flex;
   width: 100%;
-  justify-content: end;
+  justify-content: space-between;
   color: var(--Grayscale7, #000);
-  text-align: right;
   font-family: TheJamsil;
   font-size: 20px;
   font-style: normal;
   font-weight: 400;
   line-height: 30px; /* 120% */
+}
+
+.product-aggregate {
+  display: flex;
+  align-items: center;
 }
 </style>
