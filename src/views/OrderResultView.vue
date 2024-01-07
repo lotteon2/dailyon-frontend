c90f85ca733582e000028b567a95a08d9479606c
JunYong Sun <ssjy4974@naver.com>
2024-01-07 13:07:31 +0900

    [ADD] 주문 완료 후 주문 내역 확인하기 버튼 연결

--- src/views/OrderResultView.vue
+++ src/views/OrderResultView.vue
@@ -3,11 +3,14 @@ import { Result, Button } from 'ant-design-vue'
 import { useRoute } from 'vue-router'
 import { ref, onMounted } from 'vue'
 import { useProductStore } from '@/stores/product/ProductStore'
+import { getMember } from '@/apis/member/member'
+import router from '@/router'
 const productStore = useProductStore()
 const route = useRoute()
 const orderId = ref(route.params.orderId)
-onMounted(() => {
+onMounted(async () => {
   productStore.deletePinia()
+  await getMember()
 })
 </script>
 <template>
@@ -17,7 +20,9 @@ onMounted(() => {
     :sub-title="`주문 번호 : ${orderId} 최종 주문완료까지는 1 ~ 5분정도 걸릴 수 있습니다.`"
   >
     <template #extra>
-      <Button Button key="console" type="primary">주문내역 조회하기</Button>
+      <Button Button key="console" type="primary" @click="router.push('/order-history')"
+        >주문내역 조회하기</Button
+      >
     </template>
   </Result>
 </template>
