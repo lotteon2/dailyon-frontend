f5922e3e40a8f8550aa78b20a96fb0b79cb26ef8
AMolRang <rlghks131@korea.ac.kr>
2024-01-08 14:45:29 +0900

    [Feat] 토큰 검증 로직 추가

--- src/apis/utils/index.ts
+++ src/apis/utils/index.ts
@@ -1,41 +1,69 @@
 import axios, { type AxiosInstance, type InternalAxiosRequestConfig } from 'axios'
 
-// vite에서 환경변수 값을 가져오기 위해서 import.meta 를 사용함
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-// 기본 API 요청 처리
 const axiosApi = (baseURL: string) => {
   const instance: AxiosInstance = axios.create({
     baseURL,
     withCredentials: true
   })
 
-  // TODO: 로그인 정보 저장 및 API에 인증 토큰 붙이는 코드
   instance.interceptors.request.use((config) => {
     const accessToken = localStorage.getItem('accessToken')
 
-    // localStorage에 accessToken이 존재할 때만 헤더에 추가
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`
     }
 
     return config
   })
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        try {
+          const newToken = await refreshToken();
+
+          if (newToken) {
+            originalRequest.headers.Authorization = `Bearer ${newToken}`;
+            return axios(originalRequest);
+          } else {
+            // 토큰 갱신 실패 시 로그인 페이지로 리다이렉트 또는 다른 적절한 조치 수행
+            localStorage.clear();
+            alert("로그인 정보가 만료되었습니다. 다시 로그인해주세요.");
+            window.location.href = '/login';
+            return Promise.reject(error);
+          }
+        } catch (refreshError) {
+          // 토큰 갱신 중에 오류가 발생한 경우
+          console.error('Error while refreshing token:', refreshError);
+          return Promise.reject(error);
+        }
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
   return instance
 }
 
-// 인증 요청
 const axiosAuthApi = (baseURL: string) => {
   const instance: AxiosInstance = axios.create({
     baseURL,
     withCredentials: true
   })
 
-  // TODO: 로그인 정보 저장 및 API에 인증 토큰 붙이는 코드
   instance.interceptors.request.use((config) => {
     const accessToken = localStorage.getItem('accessToken')
 
-    // localStorage에 accessToken이 존재할 때만 헤더에 추가
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`
     }
@@ -43,8 +71,92 @@ const axiosAuthApi = (baseURL: string) => {
 
     return config
   })
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        try {
+          const newToken = await refreshToken();
+
+          if (newToken) {
+            originalRequest.headers.Authorization = `Bearer ${newToken}`;
+            return axios(originalRequest);
+          } else {
+            // 토큰 갱신 실패 시 로그인 페이지로 리다이렉트 또는 다른 적절한 조치 수행
+            localStorage.clear();
+            alert("로그인 정보가 만료되었습니다. 다시 로그인해주세요.");
+            window.location.href = '/login';
+            return Promise.reject(error);
+          }
+        } catch (refreshError) {
+          // 토큰 갱신 중에 오류가 발생한 경우
+          console.error('Error while refreshing token:', refreshError);
+          return Promise.reject(error);
+        }
+      }
+  
+      return Promise.reject(error);
+    }
+  );
+  
+
   return instance
 }
+
+const refreshToken = async () => {
+  try {
+    const memberString = localStorage.getItem('member');
+
+ if (memberString) {
+      const member = JSON.parse(memberString);
+      const memberId = member.memberId;
+
+      const response = await axios.post(
+        `${BASE_URL}/auth-service/auth/refresh-token`,
+        {
+          accessToken: localStorage.getItem('accessToken'),
+        },
+        {
+          headers: {
+            'MemberId': memberId,
+          },
+        }
+      );
+
+      const newAccessToken = response.data.accessToken;
+      if (newAccessToken) {
+        delete axios.defaults.headers.common['MemberId'];
+        localStorage.setItem('accessToken', newAccessToken);
+        return newAccessToken;
+      } else {
+        return null;
+      }
+    } else {
+      console.error('Member object not found in localStorage');
+      return null;
+    }
+  } catch (error) {
+    if (
+      (error as any)?.response?.status === 401 && (error as any)?.response?.data === 'RefreshToken expired'
+    ) {
+      alert("로그인 정보가 만료되었습니다. 다시 로그인해주세요.");
+      window.location.href = '/login';
+    } else {
+      // 기타 예외 처리
+      console.error('Error:', error);
+    }
+  }
+};
+
+
+
 
 export const defaultAxiosInstance: AxiosInstance = axiosApi(BASE_URL)
 export const authAxiosInstance: AxiosInstance = axiosAuthApi(BASE_URL)
