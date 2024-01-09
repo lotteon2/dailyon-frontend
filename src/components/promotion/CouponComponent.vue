60fcbc60052ad99b82284499683bb33aed795c3a
AMolRang <rlghks131@korea.ac.kr>
2024-01-09 16:55:58 +0900

    [Feat] 쿠폰 디자인 수정

--- src/components/promotion/CouponComponent.vue
+++ src/components/promotion/CouponComponent.vue
@@ -4,6 +4,8 @@ import PaginationComponent from '@/components/ootd/PaginationComponent.vue'
 import { getMyCoupons } from '@/apis/coupon/coupon'
 import type { Coupons, Coupon } from '@/apis/coupon/CouponItemDto'
 
+const VITE_STATIC_IMG_URL = ref<string>(import.meta.env.VITE_STATIC_IMG_URL)
+
 const requestPage = ref<number>(0)
 const totalPages = ref<number>(0); 
 const totalElements = ref<number>()
@@ -40,28 +42,57 @@ watch(requestPage, async (afterPage, beforePage) => {
   }
 })
 
+const formatCouponDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear() % 100;
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}/${month}/${day}`;
+};
+
+
+const formatDiscountValue = (coupon: Coupon) => {
+  if (coupon.discountType === "FIXED_AMOUNT") {
+    const formattedValue = Number(coupon.discountValue).toLocaleString();
+    return `${formattedValue}원`;
+  } else if (coupon.discountType === "PERCENTAGE") {
+    return `${coupon.discountValue}%`;
+  }
+  return coupon.discountValue;
+};
+
 </script>
 <template>
   <div class="coupon-container">
     <div class="container-title">쿠폰 관리</div>
     <div class="container-line"></div>
-    <table>
+    <table> 
+      <col width="150px" />
+      <col width="400px" />
+      <col width="200px" />
+      <col width="200px" />
       <thead>
         <tr class="coupon-table-data1">
-          <th>ID</th>
-          <th>Name</th>
-          <th>Discount Type</th>
-          <th>Discount Value</th>
-          <th>Start At</th>
+          <th></th>
+          <th>쿠폰이름</th>
+          <th>할인금액</th>
+          <th>마감일</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(coupon, index) in coupons?.memberCouponInfoReadItemResponse" :key="index" class="coupon-table-data2">
-          <td>{{ coupon.id }}</td>
+        <tr v-for="(coupon, index) in coupons?.memberCouponInfoReadItemResponse" :key="index" class="coupon-table-data2 ">
+          <td> <image
+            :href="`${VITE_STATIC_IMG_URL}${coupon.targetImgUrl}` || undefined"
+            x="0"
+            y="0"
+            width="150"
+            height="150"
+            mask="url(#circle-mask)"
+            preserveAspectRatio="xMidYMid slice"
+          /></td>
           <td>{{ coupon.name }}</td>
-          <td>{{ coupon.discountType }}</td>
-          <td>{{ coupon.discountValue }}</td>
-          <td>{{ coupon.startAt }}</td>
+          <td>{{ formatDiscountValue(coupon) }}</td>
+          <td>{{ formatCouponDate(coupon.endAt) }}</td>
         </tr>
       </tbody>
     </table>
