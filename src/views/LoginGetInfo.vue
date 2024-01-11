bd59a554e64cb7d8b08e0644f47ace4a9d2711cc
CessnaJ <eon8748@gmail.com>
2024-01-12 00:55:11 +0900

    [FIX] watch가 localStorage 감지 못하는 이슈 - LoginGetInfo에서 최초 구독 발동

--- src/views/LoginGetInfo.vue
+++ src/views/LoginGetInfo.vue
@@ -1,42 +1,43 @@
 <script setup lang="ts">
-import { authAxiosInstance } from '@/apis/utils';
-import router from '@/router';
+import { authAxiosInstance } from '@/apis/utils'
+import router from '@/router'
 import { useRoute } from 'vue-router'
-import { onMounted } from 'vue';
-import { useMemberStore } from '@/stores/member/MemberStore';
+import { onMounted } from 'vue'
+import { useMemberStore } from '@/stores/member/MemberStore'
+import { useNotificationStore } from '@/stores/notification/NotificationStore'
 
 const getMember = async () => {
   try {
-    const response = await authAxiosInstance.get('/member-service/members');
-    return response.data;
+    const response = await authAxiosInstance.get('/member-service/members')
+    return response.data
   } catch (error) {
-    console.error('API 호출 중 오류 발생:', error);
+    console.error('API 호출 중 오류 발생:', error)
   }
-};
+}
 
 onMounted(async () => {
   const route = useRoute()
   const accessToken = route.query.token as string
-  localStorage.setItem("accessToken", accessToken)
+  localStorage.setItem('accessToken', accessToken)
 
-  const memberStore = useMemberStore() as any;
+  const notificationStore = useNotificationStore()
+  const memberStore = useMemberStore() as any
   const memberInfo = await getMember()
-  
-  const deletedValue: boolean = memberInfo.deleted;
 
-  if(deletedValue){
-    localStorage.clear();
-    alert("이미 탈퇴한 계정입니다.")
-    router.push({ name:'login'})
+  const deletedValue: boolean = memberInfo.deleted
+
+  if (deletedValue) {
+    localStorage.clear()
+    alert('이미 탈퇴한 계정입니다.')
+    router.push({ name: 'login' })
   } else {
-    memberStore.setMemberInfo(memberInfo);
-    redirectToMainPage();
+    memberStore.setMemberInfo(memberInfo)
+    notificationStore.subscribeToNotificationsHandler()
+    redirectToMainPage()
   }
-
-  
-});
+})
 
 const redirectToMainPage = () => {
-  router.push({ name: 'home' });
-};
+  router.push({ name: 'home' })
+}
 </script>
