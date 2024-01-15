d5ca8c8f8a79a8cb52f529fe1e17a7885e54e9a0
JunYong Sun <ssjy4974@naver.com>
2024-01-15 16:19:05 +0900

    [FEAT] 배송 조회 모달 추가

--- src/components/gifts/ReceiverGiftListComponent.vue
+++ src/components/gifts/ReceiverGiftListComponent.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { ref, onBeforeMount } from 'vue'
-import { List, ListItem, ListItemMeta, Button } from 'ant-design-vue'
+import { List, ListItem, ListItemMeta, Button, Modal } from 'ant-design-vue'
 import { getReceiverGifts } from '@/apis/order/order'
-import { createDelivery } from '@/apis/delivery/delivery'
+import { createDelivery, getDelivery } from '@/apis/delivery/delivery'
 import type { GiftPageResponse, GiftResponse } from '@/apis/order/orderDto'
-import type { CreateDeliveryDto } from '@/apis/delivery/deliveryDto'
+import type { CreateDeliveryDto, DeliveryResponse } from '@/apis/delivery/deliveryDto'
 import GiftAddressModal from './GiftAddressModal.vue'
 const VITE_STATIC_IMG_URL = ref<string>(import.meta.env.VITE_STATIC_IMG_URL)
 const gifts = ref<GiftResponse[]>([])
+const deliveryInfo = ref<DeliveryResponse>()
 const index = ref<number>()
 const pagination = {
   onChange: async (page: number) => {
@@ -40,6 +41,18 @@ const openModal = (idx: number) => {
 const closeModal = () => {
   isModalVisible.value = false
 }
+
+const showModal = async (idx: number) => {
+  const orderNo = gifts.value[idx].orderNo
+  deliveryInfo.value = await getDelivery(orderNo)
+  open.value = true
+}
+
+const handleOk = (e: MouseEvent) => {
+  open.value = false
+}
+
+const open = ref<boolean>(false)
 
 const fill = async (addressInfo: any) => {
   if (index.value != undefined) {
@@ -86,10 +99,20 @@ const fill = async (addressInfo: any) => {
           <span>상태 : {{ item.status }}</span>
         </div>
         <Button v-if="item.status === '주문 완료'" @click="openModal(index)">수락 하기</Button>
-        <Button v-else>배송 확인</Button>
+        <Button v-else @click="showModal(index)">배송 확인</Button>
       </ListItem>
     </template>
   </List>
+  <Modal v-model:open="open" title="배송 조회" @ok="handleOk">
+    <div class="delivery-modal-p">
+      <p>받는이 : {{ deliveryInfo?.receiver }}</p>
+      <p>우편번호 : {{ deliveryInfo?.postCode }}</p>
+      <p>도로명 주소 : {{ deliveryInfo?.roadAddress }}</p>
+      <p>상세 주소 : {{ deliveryInfo?.detailAddress }}</p>
+      <p>핸드폰 번호 : {{ deliveryInfo?.phoneNumber }}</p>
+      <p>배송 상태 : {{ deliveryInfo?.status }}</p>
+    </div>
+  </Modal>
 </template>
 
 <style scoped>
@@ -105,5 +128,12 @@ const fill = async (addressInfo: any) => {
 }
 .product-name {
   padding-top: 5px;
+}
+
+.delivery-modal-p {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
 }
 </style>
